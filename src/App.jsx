4e176943ffb1ea8c2ab9ad450c4a4eef6cff05ec
Anji Liu <anjiliu219@gmail.com>
2026-05-27 19:18:44 +0800

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Toaster } from "./components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from './lib/query-client'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import Home from './pages/Home';
 import Group from './pages/Group';
